6a58d17b3b8f535c755f86d28f1342bffcb33cf7
Mr. Ydav <y.dav360@gmail.com>
2026-01-11 03:31:34 +0300

    Add InlineEditors factory

--- InlineEditors/InlineEditors.cs
+++ InlineEditors/InlineEditors.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace YDs_AwesomeDataGrid.InlineEditors
 {
     public static class InlineEditors
     {
-        // TODO: add fabric
+        public static IInlineEditor CreateByColumnType(Type type)
+        {
+            if (type.IsEnum) return new InlineEnumEditor(Enum.GetValues(type).Cast<object>().ToArray());
+            if (type == typeof(int)) return new InlineInt32Editor();
+            if (type == typeof(float)) return new InlineFloat32Editor();
+            if (type == typeof(DateTime)) return new InlineDateTimeEditor();
+            return new InlineTextEditor();
+        }
     }
 }
